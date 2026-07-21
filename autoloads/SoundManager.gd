21d27a62dfeab4766f3f86ceac8dc16f9046ff29
George-Nino-Anguluan-Jr <gnsangjr@gmail.com>
2026-07-21 18:26:34 +0800

    feat: Add music tracks and update SoundManager for dynamic audio playback

--- autoloads/SoundManager.gd
+++ autoloads/SoundManager.gd
@@ -24,12 +24,18 @@ var music_volume: float = 0.2
 var _music_player: AudioStreamPlayer
 var _current_music_level: int = -1
 
-# Root frequencies for keys (C4=261.63, each step * 2^(1/12))
-const _ROOT_FREQS: Dictionary = {
-	1: 261.63, 2: 293.66, 3: 329.63, 4: 349.23, 5: 392.00,
-	6: 440.00, 7: 493.88, 8: 523.25, 9: 587.33, 10: 659.25,
-	11: 698.46, 12: 783.99
-}
+const MUSIC_TRACKS: Array[String] = [
+	"res://assets/music/lvl_0_the_tutorial.mp3",
+	"res://assets/music/lvl_1_the_royal_palace.mp3",
+	"res://assets/music/lvl_2_the_village.mp3",
+	"res://assets/music/lvl_3_the_grassland.mp3",
+	"res://assets/music/lvl_4_the_desert.mp3",
+	"res://assets/music/lvl_5_the_oasis_or_resting_place.mp3",
+	"res://assets/music/lvl_6_the_beach.mp3",
+	"res://assets/music/lvl_7_the_raft_on_the_ocean.mp3",
+	"res://assets/music/lvl_8_the_volcanic_sea_shore.mp3",
+	"res://assets/music/lvl_9_the_volcanic_ascent.mp3",
+]
 
 func _ready() -> void:
 	process_mode = PROCESS_MODE_ALWAYS
@@ -239,76 +245,34 @@ func play_ram_spend() -> void:
 func play_level_music(level: int) -> void:
 	if level == _current_music_level and _music_player.playing:
 		return
+	var idx = (level - 1) % MUSIC_TRACKS.size()
+	var path = MUSIC_TRACKS[idx]
 	_current_music_level = level
-	var track = _generate_music_track(level)
-	if track:
-		_music_player.stop()
-		_music_player.stream = track
-		_music_player.volume_db = -16.0
-		var vol_scale = linear_to_db(music_volume) if music_volume > 0.0 else -80.0
-		_music_player.volume_db += vol_scale
-		_music_player.play()
+	var stream = _load_music(path)
+	if not stream:
+		return
+	_music_player.stop()
+	_music_player.stream = stream
+	_music_player.volume_db = -16.0
+	var vol_scale = linear_to_db(music_volume) if music_volume > 0.0 else -80.0
+	_music_player.volume_db += vol_scale
+	_music_player.play()
+
+func _load_music(path: String) -> AudioStreamMP3:
+	var file = FileAccess.open(path, FileAccess.READ)
+	if not file:
+		return null
+	var data = file.get_buffer(file.get_length())
+	file.close()
+	var stream = AudioStreamMP3.new()
+	stream.data = data
+	stream.loop = true
+	stream.loop_offset = 0.0
+	return stream
 
 func stop_music() -> void:
 	_music_player.stop()
 	_current_music_level = -1
-
-func _generate_music_track(level: int) -> AudioStreamWAV:
-	var key_idx = ((level - 1) % 12) + 1
-	var root = _ROOT_FREQS[key_idx]
-	var is_major = level % 2 == 1
-	var bpm = 90.0
-	var beats_per_loop = 8
-	var loop_duration = beats_per_loop * (60.0 / bpm)
-	var mix_rate = 22050
-	var num_samples = int(mix_rate * loop_duration)
-
-	var stream = AudioStreamWAV.new()
-	stream.mix_rate = mix_rate
-	stream.format = AudioStreamWAV.FORMAT_8_BITS
-	stream.stereo = false
-	stream.loop_mode = AudioStreamWAV.LOOP_FORWARD
-	stream.loop_begin = 0
-	stream.loop_end = num_samples
-
-	var bytes = PackedByteArray()
-	bytes.resize(num_samples)
-
-	var major_third = pow(2.0, 4.0 / 12.0)
-	var minor_third = pow(2.0, 3.0 / 12.0)
-	var fifth = pow(2.0, 7.0 / 12.0)
-	var chord = [root]
-	if is_major:
-		chord.append(root * major_third)
-		chord.append(root * fifth)
-	else:
-		chord.append(root * minor_third)
-		chord.append(root * fifth)
-
-	var amp_scale = 0.25
-
-	for i in range(num_samples):
-		var t = float(i) / mix_rate
-		var beat = t * bpm / 60.0
-		var sample = 0.0
-
-		# Bass drone (root, 2 octaves down, very soft)
-		var bass_pulse = 0.5 + 0.5 * sin(beat * PI / beats_per_loop)
-		sample += sin(2.0 * PI * root * 0.25 * t) * bass_pulse * 0.20
-
-		# Chord pad (soft, gentle fade)
-		var pad_phase = fmod(t, loop_duration) / loop_duration
-		var pad_env = 0.3 + 0.7 * sin(pad_phase * PI * 0.5)
-		for f in chord:
-			sample += sin(2.0 * PI * f * t * 0.5) * pad_env * 0.06
-
-		sample *= amp_scale
-		sample = clamp(sample, -1.0, 1.0)
-		var val = int((sample * 0.5 + 0.5) * 255.0)
-		bytes[i] = clamp(val, 0, 255)
-
-	stream.data = bytes
-	return stream
 
 func _play(stream: AudioStreamWAV, vol_db: float) -> void:
 	var player = AudioStreamPlayer.new()
